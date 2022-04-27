861d6bd9f605d5efd17433ff30a07490043f8e00
camillo.bovio <camillo@18tre.com>
2022-04-27 21:49:11 +0200

    New pages

---
+++ pages/About.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div className="h-full flex">
+      <div className="flex items-center">
+        <div>
+          <h1 className="mb-5">About</h1>
+          <p className="text-2xl">Freelance for over 10 year working</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,37 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import Head from 'next/head';
-import { Interface } from 'readline';
-
-const firstName: string = 'Camillo';
-const lastName = 'Bovio';
 
 const Home: NextPage = () => {
+  const [first, setFirst] = useState<boolean>(true);
+
   return (
     <>
       <Head>
         <title>
           Camillo Alexandre Bovio - Web designer, UI/UX & Frontend developer,
-          React JS, TypeScript
+          javascript, React JS, TypeScript based in Milano
         </title>
-        <meta name="description" content="Camillo Bovio" />
+        <meta
+          name="description"
+          content="Camillo Bovio  Web designer, UI/UX & Frontend developer,
+          javascript, React JS, TypeScript based in Milano"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <div className="ciao"></div>
-        <h1>{firstName}</h1>
+      <main className="flex items-center h-full md:pl-20">
+        <div className="text-3xl">
+          <p className="mb-2">My name is Camillo Alexandre Bovio.</p>
+          <h1 className="mb-5">
+            I’m a web designer and frontend developer <br />
+            based in Milano (Italia).
+          </h1>
+          <Link href="/About">
+            <a className="text-base hover:underline">More about me</a>
+          </Link>
+        </div>
       </main>
     </>
   );
