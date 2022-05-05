707ac29845f6e727122cd4d71989ab59ba90de3d
camillo.bovio <camillo@18tre.com>
2022-05-05 17:29:16 +0200

    Improved navbar and menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,5 +1,67 @@
+import Link from 'next/link';
+import { useState } from 'react';
+import { HamburgerIcon } from './HamburgerIcon';
+
+interface MenuListItem {
+  name: string;
+  href: string;
+}
+
+const menuList: MenuListItem[] = [
+  {
+    name: 'Home page',
+    href: '/',
+  },
+  {
+    name: 'About',
+    href: '/about',
+  },
+  {
+    name: 'Skills',
+    href: '/skills',
+  },
+  {
+    name: 'Portfolio',
+    href: '/projects',
+  },
+  {
+    name: 'Contact',
+    href: '/contact',
+  },
+];
+
+export const NavbarRight = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <div>
+      <HamburgerIcon isOpen={isOpen} setIsOpen={setIsOpen} />
+      {/* Desktop menu */}
+      <ul className="hidden gap-5 lg:flex">
+        <Menu />
+      </ul>
+      {/* Mobile menu */}
+      {isOpen && (
+        <div className="fixed top-0 left-0 flex items-center w-screen h-screen bg-white lg:hidden">
+          <ul>
+            <Menu />
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <>
+      {menuList.map(item => (
+        <Link href={item.href} key={item.name}>
+          <a>{item.name}</a>
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default Menu;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,12 @@
 import React, { useState } from 'react';
-import Link from 'next/link';
-import { HamburgerIcon } from './HamburgerIcon';
-import Menu from './Menu';
+import { NavbarRight } from './Menu';
+import Logo from './Logo';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const handleClick = () => setIsOpen(true);
-
   return (
-    <div className="p-5 flex justify-between">
-      <div>
-        <Link href="/">
-          <a>Camillo Alexandre Bovio</a>
-        </Link>
-      </div>
-      <HamburgerIcon isOpen={isOpen} setIsOpen={setIsOpen} />
+    <div className="flex justify-between p-5">
+      <Logo />
+      <NavbarRight />
     </div>
   );
 };
