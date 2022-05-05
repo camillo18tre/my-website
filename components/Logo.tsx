4c213adee6e8f0f969827d4c1c5a285e3fd8111b
camillo.bovio <camillo@18tre.com>
2022-05-05 17:28:55 +0200

    New Logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function Logo() {
+  return (
+    <div>
+      <Link href="/">
+        <a>Camillo Alexandre Bovio</a>
+      </Link>
+    </div>
+  );
+}
+
+export default Logo;
